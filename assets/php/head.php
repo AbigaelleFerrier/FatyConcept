5eda4a391a94740054fb5c8186d8440b07fc0250
Cirill Ferrier <cirill@asheart.fr>
2018-09-04 16:55:09 +0200

    a

--- assets/php/head.php
+++ assets/php/head.php
@@ -3,7 +3,7 @@
 
 <!--Import materialize.css-->
 <link type="text/css" rel="stylesheet" href="assets/css/main.css">
-<link type="text/css" rel="stylesheet" href="assets/css/materialize.min.css"  media="screen,projection"/>
+<link type="text/css" rel="stylesheet" href="assets/css/materialize.css"  media="screen,projection"/>
 
 <!--Let browser know website is optimized for mobile-->
 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
--- assets/php/header.php
+++ assets/php/header.php
@@ -1,7 +1,9 @@
-<div class="container" id="header">
-	<div class="row">
-		<div class="col s10 offset-s1">
-			<h1>FATY CONCEPT</h1>
+<div id="header">
+	<div class="container">
+		<div class="row">
+			<div class="col s10 offset-s1">
+				<h1>FATY CONCEPT</h1>
+			</div>
 		</div>
 	</div>
 </div>
